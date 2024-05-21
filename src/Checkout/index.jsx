f1d9752f3455af73ea21c176c7ca0e08d31b4a6e
Gulshair <gulshairawan635@gmail.com>
2024-05-21 14:25:28 +0500

    Some Minor Chnages

--- src/Checkout/index.jsx
+++ src/Checkout/index.jsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-import CheckoutForm from './CheckoutForm';
+import React, { useEffect, useState } from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Checkout from "./Checkout";
 
-const stripePromise = loadStripe('pk_test_51PH2dsKvSuAOYrcIrXNxqCVOCHU5NyayZYrvKU7nuIdjr9kMwCp0isRg1Yl0iU7rT61prQXbihPObrWpV2qYuGbi00zoyhtZBf');
+const stripePromise = loadStripe(
+  "pk_test_51PH2dsKvSuAOYrcIrXNxqCVOCHU5NyayZYrvKU7nuIdjr9kMwCp0isRg1Yl0iU7rT61prQXbihPObrWpV2qYuGbi00zoyhtZBf"
+);
 
 export default function App() {
-  const [clientSecret, setClientSecret] = useState('');
+  const [clientSecret, setClientSecret] = useState("");
 
   useEffect(() => {
     // Fetch the client secret from the backend
-    fetch('/api/create-payment-intent', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ amount: 1000 }) // replace with the actual amount
+    fetch("/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ amount: 1000 }), // replace with the actual amount
     })
       .then((response) => response.json())
       .then((data) => setClientSecret(data.clientSecret));
@@ -26,7 +28,7 @@ export default function App() {
   return (
     clientSecret && (
       <Elements stripe={stripePromise} options={options}>
-        <CheckoutForm />
+        <Checkout />
       </Elements>
     )
   );
